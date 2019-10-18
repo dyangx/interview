c84beb497bbbc38e897ddaf541368beb24c476a2
yangjie <yangjie@yangjie@rivamed.cn>
2019-10-18 17:08:43 +0800

    init

---
+++ src/main/java/com/example/mapper/TesterMapper.java
@@ -0,0 +1,8 @@
+package com.example.mapper;
+
+/**
+ * @author: yangjie
+ * @date: Created in 2019/10/18 14:53
+ */
+public class testerMapper {
+}
---
+++ src/main/java/com/example/vo/Usr.java
@@ -0,0 +1,8 @@
+package com.example.vo;
+
+/**
+ * @author: yangjie
+ * @date: Created in 2019/10/18 14:55
+ */
+public class Usr {
+}
