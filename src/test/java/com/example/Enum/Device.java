0b192dc4c89d6856dc631d0fea5c3fe66b8c2b65
yangjie <yangjie@yangjie@rivamed.cn>
2020-04-15 08:33:55 +0800

    light“

--- src/test/java/com/example/Enum/Device.java
+++ src/test/java/com/example/Enum/Device.java
@@ -5,4 +5,44 @@ package com.example.Enum;
  * @date: Created in 2020/3/11 16:03
  */
 public enum Device {
+    CST_CABINET("001","耗材柜"),
+    VERIFICATION_AREA("005","核验区"),
+    PASSAGEWAY_DOOR("008","通道门");
+
+    private final String code;
+    private final String name;
+
+    Device(String code,String name){
+        this.code = code;
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Device getDevice(String name){
+        if (name == null){
+            return null;
+        }
+        if(name.contains(CST_CABINET.name)){
+            return CST_CABINET;
+        }else if (name.contains(VERIFICATION_AREA.name)){
+            return VERIFICATION_AREA;
+        }else if (name.contains(PASSAGEWAY_DOOR.name)){
+            return PASSAGEWAY_DOOR;
+        }else {
+            return null;
+        }
+    }
+
+}
+class Test2{
+    public static void main(String[] args) {
+        System.out.println(Device.getDevice("通道门").getName());
+    }
 }
--- src/test/java/com/example/mainTest/Test6.java
+++ src/test/java/com/example/mainTest/Test6.java
@@ -4,6 +4,11 @@ import com.example.service.TheadQueryService;
 import com.example.utils.HttpRequestUtil;
 import com.example.utils.TestYouUtil;
 import com.example.vo.User;
+import org.springframework.beans.BeanUtils;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: yangjie
@@ -14,13 +19,33 @@ public class Test6 {
     static TheadQueryService theadQueryService;
 
     public static void main(String[] args) {
-        User u = new User();
-        u.setAge("1");
-        u.setId("000000");
-        u.setName("张三");
-
-        String s = TestYouUtil.postReq("userService","printUser", u);
-        System.out.println(s);
+        User u = new User("11","222","we");
+        Map<String,Object> map = new HashMap<>();
+        System.out.println(map);
+        System.out.println("\033[1;93;45m" + "就是酱紫的"+"\033[m");
+        System.out.println("\033[5m" + "就是酱紫的");
+        System.out.println("\033[31m" + "就是酱紫的");
+        System.out.println("\033[32m" + "就是酱紫的-------------");
+        System.out.println("\033[33m" + "就是酱紫的");
+        System.out.println("\033[34m" + "就是酱紫的");
+        System.out.println("\033[35m" + "就是酱紫的");
+        System.out.println("\033[36m" + "就是酱紫的ssdss");
+        System.out.println("\033[37m" + "就是酱紫的");
+        System.out.println("\033[38m" + "就是酱紫的");
+        System.out.println("\033[39m" + "就是酱紫的");
+        System.out.println("\033[0;32m" + "就是酱紫的");
+        System.out.println("\033[0;5m" + "就是酱紫的\033[0m");
+        System.out.println("\033[46m" + "就是酱紫的"+"\033[m");
+        System.out.println("------------------------------");
+        System.out.println("\033[0;30;42m color!!! \033[0m Hello \n");
+        System.out.println("\033[0;30;40m color!!! \033[0m Hello \n");
+        System.out.println("\033[0;30;41m color!!! \033[0m Hello \n");
+        System.out.println("\033[0;30;42m color!!! \033[0m Hello \n");
+        System.out.println("\033[0;30;43m color!!! \033[0m Hello \n");
+        System.out.println("\033[0;30;44m color!!! \033[0m Hello \n");
+        System.out.println("\033[0;30;45m color!!! \033[0m Hello \n");
+        System.out.println("\033[0;30;46m color!!! \033[0m ");
+        System.out.println("\033[0;30;47m color!!!\n");
     }
 
 
